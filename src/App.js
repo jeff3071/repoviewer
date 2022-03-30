c05bf56c36dc111dd17cd2a0932754c3a403184d
jeff3071 <jeff563715@gmail.com>
2022-03-30 14:21:12 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Link , Switch} from "react-router-dom"
+import user from "./components/user.js"
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputvalue: '',
+    }
+  }
+
+  handleclick() {
+    console.log(this.state)
+    fetch( 'https://api.github.com/users/'+this.state.inputvalue+'/repos',{method:"GET"})
+    .then(res => res.json())
+    .then((data )=> {
+      console.log(data);
+    })
+    .catch(e => {
+      console.log(e);
+    })
+  }
+
+  render () {
+    return (
+      <BrowserRouter>
+        <input type="text" onChange={(e) => {this.state.inputvalue=e.target.value}}></input>
+        {/* <button onClick={() => this.handleclick()}>取得</button> */}
+        <ul>
+          <li><Link to="users">查詢</Link></li>
+        </ul>
+        <Switch>
+          <Route path='/users' component={user}/>
+        </Switch>
+      </BrowserRouter>
+    )}; 
 }
-
 export default App;
---
+++ src/components/user.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class user extends React.Component {
+  render () {
+    return (
+      <div>hihi</div>
+    )
+  }
+}
+
+export default user
